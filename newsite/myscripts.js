30876ac1083ac9171ba661b5b83552ec6935783d
pyr0mind <114652672+pyr0mind@users.noreply.github.com>
2025-06-23 15:29:06 +0530

    Delete newsite directory

--- newsite/myscripts.js
+++
@@ -1,8 +0,0 @@
-
-    // Email form submission handler
-    document.querySelector('.waitlist form').addEventListener('submit', function (e) {
-      e.preventDefault();
-      const email = e.target.querySelector('input').value;
-      alert(`🎉 Thank you, ${email}! 🚀 We’re excited to launch on 20th October. Stay tuned! ✨`);
-      e.target.reset();
-    })
